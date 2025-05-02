b2de65ccdb922dd04cc1bbe1277d4a862330c530
Bhuyan Md Anowarul Karim <anowarul.karim@bjitgroup.com>
2025-05-02 16:16:45 +0600

    banner

--- P1705_VendorBid/__manifest__.py
+++ P1705_VendorBid/__manifest__.py
@@ -20,7 +20,7 @@
     # any module necessary for this one to work correctly
     'depends': ['purchase', 'website','contacts'],
     'license': 'LGPL-3',
-
+    'images': ['static/description/banner.gif'],
     # always loaded
     'data': [
         'security/supplies_security.xml',
